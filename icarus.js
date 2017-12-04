8f35223ee091e2d3bd4a4b0282970a0c756751c6
diaphora <32223054+diaphora@users.noreply.github.com>
2017-12-03 21:18:56 -0600

    Final Check on Branch Contents before Submission

--- icarus.js
+++ icarus.js
@@ -1,52 +1,13 @@
-//Header element defers JavaScript loading until after HTML: <script><script src="icarus.js" script type="text/javascript" defer>
-//Or, alternatively, loading simultaneously: <script><script src="icarus.js" script type="text/javascript" async>
+$(document).ready(function(){
+    $("a").gray-tooltip();
+});
 
-    //The purpose of this JavaScript code is to produce interactive text balloons that do not deter from the site's function or its ability to serve the needs of the user.
+$(document).ready(function(){
+    $("a").black-tooltip();
+});
 
-function mouseoverNo() {
-        var popup = document.getElementById("BinaryPopup1");
-        popup.classList.toggle("show") popup.text:"You know who else read books? John Brown. Just sayin'."};
-}
-
-//Model for MouseoverNo object HTML: <element onmouseover="mouseoverNo()"><div class="popup" onclick="mouseoverNo()"><span>
-
-object.onmouseover = function mouseoverNo() {
-        var popup = document.getElementById("BinaryPopup1");
-        popup.classList.toggle("show") popup.text:"You know who else read books? John Brown. Just sayin'."};
-}
-
-    function mouseoverNo() {
-      var popup = document.getElementById("BinaryPopup1");
-        popup.classList.toggle("show") popup.text:"You know who else read books? John Brown. Just sayin'."};
-}
-
-//You know who else read books? Gandhi. Just sayin'.
-
-function mouseoverYes() {
-  var popup = document.getElementById("BinaryPopup2");
-  popup.classList.toggle("show"); popup.text:"Well, thanks for being you!"};
-}
-
-object.onmouseover = function mouseoverYes() {
-        var popup = document.getElementById("BinaryPopup2");
-        popup.classList.toggle("show"); popup.text:"Well, thanks for being you!"};
-
-//Model for MouseoverYes object HTML: <element onmouseover="mouseoverYes()"><div class="popup" onclick="mouseoverYes()"><span>
-
-    function mouseoverYes() {
-      var popup = document.getElementById("BinaryPopup2");
-        popup.classList.toggle("show") popup.text:"Well, thanks for being you!"};
-}
-
-//Well, thanks for being you!
-
-//
-
-//Client Content for Opentips:
-//A book isn't the WORST thing you could buy online.
-//What are you supposed to be doing with two dollars anyway?
-//You shouldn't expect applause just yet.
-
-function Opentip(){style: "light", target: true, showOn:"mouseover", borderWidth: 0, stemLength: 18, stemBase: 20, borderRadius:18, delay:0.4}
-
-///Model for Opentip in HTML:<div id="dark1" class="test" >Dark styles without title, hover to show</div><script type="text/javascript">function Opentip("#dark1", "You shouldn't expect applause just yet.", {style: "dark", target: true, });</script>
+if (window.parent && window.parent.parent){
+  window.parent.parent.postMessage(["resultsFrame", {
+    height: document.body.getBoundingClientRect().height,
+    slug: "2htZe"
+  }], "*"
